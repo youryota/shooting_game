5f04fc29cfe7544b014173af24c649fb03bca4d5
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2024-02-06 14:17:18 +0900

    [add]プレイヤーの移動と制限

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -19,5 +19,37 @@ class MainScene extends Phaser.Scene {
     {
         const background = this.add.image(D_WIDTH / 2, D_HEIGHT / 2, 'background');
         background.setDisplaySize(D_WIDTH, D_HEIGHT);
+        const player = this.physics.add.sprite(D_WIDTH / 2,700, 'player');
+        this.player = player;
+        this.physics.world.setBounds(0, 0, D_WIDTH, D_HEIGHT);
+        player.setCollideWorldBounds(true);
+    }
+
+    arrow_move(cursors, object){
+    
+        if(cursors.up.isDown){
+            console.log("Up!!");
+            object.setVelocityY(-400);// 上方向の速度を設定
+            
+        }else if(cursors.down.isDown){
+            console.log("down!!");
+            object.setVelocityY(400);// 下方向の速度を設定
+    
+        }else if(cursors.left.isDown){
+            console.log("Left");
+            object.setVelocityX(-400);// 左方向の速度を設定
+    
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object.setVelocityX(400);// 右方向の速度を設定
+    
+        }else{
+            object.setVelocity(0,0);// 横方向の速度を0
+        }
+    }
+
+    update(time, delta) {
+        let cursors = this.input.keyboard.createCursorKeys();
+        this.arrow_move(cursors, this.player);
     }
 }
